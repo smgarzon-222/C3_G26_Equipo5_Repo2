c99b09530529a4af666712bf7e2105c88df2c674
JavierGP25 <jpdaplus@gmail.com>
2022-08-27 00:46:46 -0500

    P1

--- TorneoG26/Torneo.App/Torneo.App.Dominio/Entidades/DirectorTecnico.cs
+++ TorneoG26/Torneo.App/Torneo.App.Dominio/Entidades/DirectorTecnico.cs
@@ -2,7 +2,8 @@ namespace Torneo.App.Dominio
 {
     public class DirectorTecnico
     {
-        //comentario de prueba
+        //comentario de prueba 270822
+        
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Documento { get; set; }
