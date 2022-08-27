4b6037a8bb047a38ea2eabf3ff93af9f1fdb55c0
claudiamilena <clamimar@gmail.com>
2022-08-26 23:27:45 -0500

    pruebaclaudia

--- C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/DirectorTecnico.cs
+++ C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/DirectorTecnico.cs
@@ -3,6 +3,7 @@ namespace Torneo.App.Dominio
     public class DirectorTecnico
     {
         public int Id { get; set; }
+        // prueba claudia
         public string Nombre { get; set; }
         public string Documento { get; set; }
         public string Telefono { get; set; }
