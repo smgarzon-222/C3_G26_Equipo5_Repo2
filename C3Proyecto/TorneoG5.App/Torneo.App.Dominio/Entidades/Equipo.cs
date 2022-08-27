b18832ddb99e0d099b5dc5c16127e82d6371af68
smgarzon-222 <smgarzon222@gmail.com>
2022-08-26 22:21:48 -0500

    comentario numero 2 public

--- C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/Equipo.cs
+++ C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/Equipo.cs
@@ -2,8 +2,8 @@ namespace Torneo.App.Dominio
 {
     public class Equipo
     {
-        //Esto es un comentario del reto
-        public int Id { get; set; }
+        
+         int Id { get; set; }
         public string Nombre { get; set; }
         public Municipio Municipio { get; set; }
         public DirectorTecnico DirectorTecnico { get; set; }
