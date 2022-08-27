82fae4d5a921f47ed9681b5d7642cd9a404fa948
smgarzon-222 <smgarzon222@gmail.com>
2022-08-26 22:19:20 -0500

    Modificacion prueba de codigo

--- C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/Equipo.cs
+++ C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/Equipo.cs
@@ -2,6 +2,7 @@ namespace Torneo.App.Dominio
 {
     public class Equipo
     {
+        //Esto es un comentario del reto
         public int Id { get; set; }
         public string Nombre { get; set; }
         public Municipio Municipio { get; set; }
