447a2d2934eb35d159e74e01b6ab2485e53e3c9a
smgarzon-222 <smgarzon222@gmail.com>
2022-08-26 22:41:00 -0500

    comentario 1

--- C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/Equipo.cs
+++ C3Proyecto/TorneoG5.App/Torneo.App.Dominio/Entidades/Equipo.cs
@@ -3,7 +3,7 @@ namespace Torneo.App.Dominio
     public class Equipo
     {
         
-         int Id { get; set; }
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public Municipio Municipio { get; set; }
         public DirectorTecnico DirectorTecnico { get; set; }
