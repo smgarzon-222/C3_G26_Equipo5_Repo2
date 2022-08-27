660a050d07bf3209d6d98062cabab3bf24dec8a8
Johan_Moreno <111515218+smgarzon-222@users.noreply.github.com>
2022-08-27 00:21:53 -0500

    Add files via upload

---
+++ TorneoG26/Torneo.App/Torneo.App.Dominio/Entidades/DirectorTecnico.cs
@@ -0,0 +1,11 @@
+namespace Torneo.App.Dominio
+{
+    public class DirectorTecnico
+    {
+        //comentario de prueba
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Documento { get; set; }
+        public string Telefono { get; set; }
+    }
+}
